d2f1856c58eb9f3b3379edd973800757a97372d5
Rein Appeldoorn <reinzor@gmail.com>
2016-04-11 15:26:56 +0200

    working connection information

--- src/robot.py
+++ src/robot.py
@@ -1,13 +1,27 @@
+import rospy, sys
 from parts.part import Part
 
 
 class Robot(object):
 
     def __init__(self, name):
+        # Store the robot_name
         self._name = name
+        self._parts = []
 
-    def get_robot_name(self):
+        # Error if we are using a namespace
+        ns = rospy.get_namespace()
+        if ns != "/":
+            rospy.logerr("The robot api instance should not be created within a namespace, exit 1")
+            sys.exit(1)
+
+    @property
+    def name(self):
         return self._name
+
+    @property
+    def connection_information(self):
+        return {part.name: part.connection_information for part in self._parts}
 
     def _setup_part(self, part_class, part_name, part_configuration={}):
         # Check if the part_class inherits from class
@@ -16,9 +30,10 @@ class Robot(object):
 
         # Construct the part
         try:
-            part = part_class(part_name, part_configuration)
+            part = part_class(self._name, part_name, part_configuration)
         except Exception as e:
             raise Exception("Failed to setup part '%s': %s" % (part_name, e))
 
         setattr(self, part_name, part)
+        self._parts.append(part)
 
